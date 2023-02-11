061e2a331cd35afc184deb962dd435bcd765c67c
takeshi tsuruda <lazywayofcat@gmail.com>
2023-02-11 20:45:45 +0900

    ホーム画面作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+
+  has_many :documents, dependent: :destroy
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+        :recoverable, :rememberable, :validatable, :confirmable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "home#index"
+  get "home/top", to: "home#top"
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
@@ -9,10 +11,12 @@ Rails.application.routes.draw do
   end
 
 
-  root to: 'documents#index'
+  # root to: 'documents#index'
 
   # ログイン関連
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+
+  resources :users, only: %i[show]
 end
