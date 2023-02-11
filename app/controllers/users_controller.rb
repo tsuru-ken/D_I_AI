98a6c4e0afd94b9d34047dc194e06a69d50779ff
takeshi tsuruda <lazywayofcat@gmail.com>
2023-02-12 00:41:40 +0900

    user/show画面の表示をメンターに相談

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  
+  def show
+  # binding.pry
+    @user = User.find(params[:id])
+    # @user = User.find_by(id: params[:id])
+  end
 end
+# private
+
+  # def admin_user?
+  #   return if current_user.admin? || User.find(params[:id]) == current_user
+  #   redirect_to root_path, alert: "他のユーザーページにはアクセスできません。"
+  # end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ Rails.application.routes.draw do
   root "home#index"
   get "home/top", to: "home#top"
 
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  resources :users, only: :show
+
   # OCR関連
   resources :documents do
     get 'texts', to: 'documents#execute_ocr'
     get 'vision_texts', to: 'documents#execute_vision_api'
   end
+
+
 
 
   # root to: 'documents#index'
@@ -18,5 +23,7 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
-  resources :users, only: %i[show]
 end
+
+
+
