7a1ab1f2a3bb39df71d59083fcc22916c904b0bb
takeshi tsuruda <lazywayofcat@gmail.com>
2023-02-14 00:00:50 +0900

    partnerとcostのlabel実装完了

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,7 +1,10 @@
 class Partner < ApplicationRecord
   has_many :partner_service_content_labels, dependent: :destroy
   has_many :partner_ai_category_labels, dependent: :destroy
+
   has_many :partner_cost_labels, dependent: :destroy
+  
+  has_many :costs, through: :partner_cost_labels
   belongs_to :user
 
 
