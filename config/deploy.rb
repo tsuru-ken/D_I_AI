9439fc2ec9eb151af141a313af9b6c12166882c5
takeshi tsuruda <lazywayofcat@gmail.com>
2023-02-19 23:31:15 +0900

    AWSデプロイ

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url, 'https://github.com/tsuru-ken/D_I_AI'
 set :branch, ENV['BRANCH'] || 'main'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/DIAI'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
