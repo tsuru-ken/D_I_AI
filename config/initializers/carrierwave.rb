4bb65cbc3f7cedf5ff3f2e0d514a5f1dce34c798
takeshi tsuruda <lazywayofcat@gmail.com>
2023-02-20 23:28:49 +0900

    S3の設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,8 +14,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      aws_access_key_id:     ENV["AKIAWJ4M6EMMHXKN2F7G"],
+      aws_secret_access_key: ENV["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"],
       region:                'ap-northeast-1',
       path_style:            true,
   }
@@ -26,12 +26,12 @@ CarrierWave.configure do |config|
   case Rails.env
     when 'production'
       config.fog_directory = '本番環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/diai'
     when 'development'
       config.fog_directory = '開発環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/diai'
     when 'test'
       config.fog_directory = 'テスト環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/テスト環境用のバケット名'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/diai'
   end
 end
